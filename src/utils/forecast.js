bb9cb0c5cc9634d4ed724dc6693d202db0189fdc
vishalsetti <vishalsetti2000@gmail.com>
2021-10-29 13:57:07 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location!");
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            console.log(body.current);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.
+            Humidity is ${body.current.humidity}%.`)
         }
     })
 }
